b82c46e8bc2ceee446c580aa330d1a109ad86445
Zaar <zaarinc@gmail.com>
2023-05-30 17:14:55 +0300

    4th seminar HW added

--- Seminar_4/HW/HW01.py
+++ Seminar_4/HW/HW01.py
@@ -4,3 +4,22 @@
 встречаются в обоих наборах.
 Пользователь вводит 2 числа. n - кол-во элементов первого множества. m - кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 """
+
+import random
+
+generator_min = int(input("Please, enter minimum number for generator:\n "))
+generator_max = int(input("Please, enter maximum number for generator:\n "))
+
+list_length_1 = int(input("Please, enter first list size:\n "))
+disordered_list_1 = [random.randint(generator_min, generator_max) for _ in range(list_length_1)]
+print(disordered_list_1)
+sorted_list_1 = set(disordered_list_1)
+
+list_length_2 = int(input("Please, enter second list size:\n "))
+disordered_list_2 = [random.randint(generator_min, generator_max) for _ in range(list_length_2)]
+print(disordered_list_2)
+sorted_list_2 = set(disordered_list_2)
+
+result = sorted(sorted_list_1.intersection(sorted_list_2))
+
+print(f'The numbers that occur in both lists are -> {result}')
--- Seminar_4/HW/HW02.py
+++ Seminar_4/HW/HW02.py
@@ -9,3 +9,40 @@
 Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом
 заданной во входном файле грядки.
 """
+
+#Т.к. задача относительно несложная, здесь я попробовал заморочиться с проверками. Возможно в данном случае что-то оказалось избыточным, но решил попробовать разобраться в вариантах отладки
+
+import random
+
+def user_input(generator_min=None, generator_max=None, bushes_quantity=None):
+    generator_min = input('Please, enter minimum number for berries generator:\n ') if generator_min is None or not generator_min.isdigit() else generator_min
+    generator_max = input('Please, enter maximum number for berries generator:\n ') if generator_max is None or not generator_max.isdigit() else generator_max
+    bushes_quantity = input('Please, enter quantity of bushes:\n ') if bushes_quantity is None or not bushes_quantity.isdigit() else bushes_quantity
+    try:
+        generator_min, generator_max, bushes_quantity = int(generator_min), int(generator_max), int(bushes_quantity)
+    except ValueError:
+        print('Error! Please, try again:')
+        generator_min, generator_max, bushes_quantity = user_input(generator_min, generator_max, bushes_quantity)
+    finally:
+        return generator_min, generator_max, bushes_quantity
+
+generator_min, generator_max, bushes_quantity = user_input()
+
+if bushes_quantity <= 2:
+    print('Error! Quantity of the bushes must be > 2!')
+elif generator_min <= 0 or generator_max <= 0:
+    print('Error! Quantity of berries cannot be a negative number!')
+elif generator_min > generator_max:
+    print('Error! Minimum number for berries generator must be less or equal to maximum number for berries generator!')
+else:        
+    ridge_file = [random.randint(generator_min, generator_max) for _ in range(bushes_quantity)]
+    print(f'Our ridge looks like this:\n {ridge_file}')
+
+    max_berries = 0
+    for i in range(bushes_quantity):
+        j = 0 if i + 1 == bushes_quantity else i + 1
+        sum = ridge_file[i] + ridge_file[j] + ridge_file[i-1]
+        if sum > max_berries:
+            max_berries = sum
+            optimum_bush_index = i
+    print(f'Module will pick {max_berries} berries and for this it must begin from the bush placed on {optimum_bush_index + 1} position')
