6ea02a017eb3e1191baeb7b2aab0f35823f6a03b
Zaar <zaarinc@gmail.com>
2023-06-06 12:12:21 +0300

    6th seminar HW added

--- Seminar_6/HW/HW01.py
+++ Seminar_6/HW/HW01.py
@@ -7,3 +7,16 @@
 Ввод: 7 2 5
 Вывод: 7 9 11 13 15
 """
+
+first_element_a = int(input("Please, enter progression 1st elem:\n "))
+step_d = int(input("Please, enter progression step_d:\n "))
+array_length = int(input("Please, enter progression array_length:\n "))
+
+progression_array = []
+
+current = first_element_a
+for n in range(1, array_length + 1):
+    current = first_element_a + (n - 1) * step_d
+    progression_array.append(current)
+ 
+print(f'Formed progression array -> {progression_array}')
--- Seminar_6/HW/HW02.py
+++ Seminar_6/HW/HW02.py
@@ -3,9 +3,31 @@
 Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не
 меньше заданного минимума и не больше заданного максимума)
 
-Ввод: [-5, 9, 0, 3, -1, -2, 1,
-4, -2, 10, 2, 0, -9, 8, 10, -9,
-0, -5, -5, 7]
+1-й пример
+Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 
 Вывод: [1, 9, 13, 14, 19]
+
+2-й пример
+Ввод: [10, 25, 7, 23, 45]
+начальное значение: 20
+конечное значение: 40
+Вывод: [1, 3]
 """
+
+import random
+
+array_length = int(input("Please, enter array length for generation:\n "))
+array = [random.randint(-10, 10) for _ in range(array_length)]
+
+print(array)
+
+range_start = int(input("Please, enter start number of the range:\n "))
+range_end = int(input("Please, enter end number of the range:\n "))
+
+array_indices = []
+for i in range(array_length):
+    if array[i] >= range_start and array[i] <= range_end:
+        array_indices.append(i)
+
+print(f'Formed array with matching to conditions indices -> {array_indices}')
